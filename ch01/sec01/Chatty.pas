a8d982f8053f6c87c07b73ba9df8d44a48050817
David Miserak <David.Miserak@gmail.com>
2020-03-02 16:51:13 -0500

    ch01: added make files in sections and did re-org.

--- ch01/sec01/Chatty.pas
+++ ch01/sec01/Chatty.pas
@@ -4,7 +4,7 @@ begin
 	write   ('A fine romance, '); {These lines are printed...}
 	writeln ('with no kisses.');  {...on the same output line.}
 	writeln; 		              {This ends the line with a carriage return.}
-	writeln; 		              {These put two blank lines in the output,}
+	writeln; 		              {These put two blank lines in the output.}
 	writeln;
 	write   ('A fine romance, ');
 	writeln ('my friend, this is!')
