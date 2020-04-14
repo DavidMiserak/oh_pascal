75b6ea15e3db2e1388b562042a858a090b0e35c4
David Miserak <David.Miserak@gmail.com>
2020-04-14 14:54:57 -0400

    ch06: added makefiles

--- ch06/sec02/ChCount.pas
+++ ch06/sec02/ChCount.pas
@@ -2,7 +2,7 @@ procedure ChCount (var Count: integer);
 {Counts the number of letters in a sentence that ends with ., ?, or !.}
 var Ch: char;
 begin
-	Cont := 0;
+	Count := 0;
 	read(ch); {Approach the bound.}
 	while(Ch <> '.') and (Ch <> '?') and (Ch <> '!'); do begin
 		Count := Count + 1; {If we're in the loop, increment the count.}
