2b1bf99237f0e4cc9d87a4ef9fef79b492157a09
David Miserak <David.Miserak@gmail.com>
2020-04-14 15:52:39 -0400

    ch12: added makefiles

--- ch12/sec01/TestRand.pas
+++ ch12/sec01/TestRand.pas
@@ -8,13 +8,15 @@ type Data = array [1 .. 10] of integer;
 var Counter, Temp: integer;
     TestBed: Data;
 
-function Random: real;
+function Random(Seed: integer): real;
 	{Returns a real in the range 0.0 .. 0.999...}
-	var Count: integer;
-	    Sum: real;
-	begin
-		{It's code details aren't important.}
+	{Returns a pseudorandom number such that 0.0 <= Random < 1.0.}
+	const MODULUS = 2027; {Values suggested by Warford.}
+	      MULTIPLIER = 15;
 
+	begin
+		Seed := (Seed * MULTIPLIER) mod MODULUS;
+		Random := Seed / MODULUS
 	end; {Random}
 
 begin {main program}
@@ -22,7 +24,7 @@ begin {main program}
 		TestBed[Counter] := 0;
 	end; {The output counter is initialized.}
 	for Counter := 1 to NUMBERofTrials do begin
-		Temp := trunc(1 + (Random * 10)); {i.e. 1 .. 10}
+		Temp := trunc(1 + ((Random(Counter)) * 10)); {i.e. 1 .. 10}
 		TestBed[Temp] := TestBed[Temp] + 1;
 	end; {We've counted appearances of 10,000 numbers.}
 	writeln('Random test distribution:');
