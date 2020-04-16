65e608015ff5ec36f92e4cf454234e68fb430845
David Miserak <David.Miserak@gmail.com>
2020-04-16 10:39:39 -0400

    ch12: formatted via ptop

--- ch12/sec01/TestRand.pas
+++ ch12/sec01/TestRand.pas
@@ -1,40 +1,50 @@
-program TestRand (input, output);
+
+Program TestRand(input, output);
 {Checks the distribution of the Random function's output.}
 
-const NUMBERofTrials = 10000;
+Const 
+  NUMBERofTrials = 10000;
 
-type Data = array [1 .. 10] of integer;
+Type 
+  Data = array [1 .. 10] Of integer;
 
-var Counter, Temp: integer;
-    TestBed: Data;
+Var 
+  Counter, Temp : integer;
+  TestBed       : Data;
 
-function Random(Seed: integer): real;
-	{Returns a real in the range 0.0 .. 0.999...}
-	{Returns a pseudorandom number such that 0.0 <= Random < 1.0.}
-	const MODULUS = 2027; {Values suggested by Warford.}
-	      MULTIPLIER = 15;
+Function Random(Seed : integer) : real;
+ {Returns a real in the range 0.0 .. 0.999...}
+ {Returns a pseudorandom number such that 0.0 <= Random < 1.0.}
 
-	begin
-		Seed := (Seed * MULTIPLIER) mod MODULUS;
-		Random := Seed / MODULUS
-	end; {Random}
+Const 
+  MODULUS = 2027; {Values suggested by Warford.}
+  MULTIPLIER = 15;
 
-begin {main program}
-	for Counter := 1 to 10 do begin
-		TestBed[Counter] := 0;
-	end; {The output counter is initialized.}
-	for Counter := 1 to NUMBERofTrials do begin
-		Temp := trunc(1 + ((Random(Counter)) * 10)); {i.e. 1 .. 10}
-		TestBed[Temp] := TestBed[Temp] + 1;
-	end; {We've counted appearances of 10,000 numbers.}
-	writeln('Random test distribution:');
-	for Counter := 1 to 10 do begin
-		write(Counter: 3);
-		write('''s')
-	end; {The heading is printed.}
-	writeln;
-	for Counter := 1 to 10 do begin
-		write(TestBed[Counter]: 5)
-	end; {The counts are printed.}
-	writeln
-end. {TestRand}
+Begin
+  Seed   := (Seed * MULTIPLIER) Mod MODULUS;
+  Random := Seed / MODULUS
+End; {Random}
+
+Begin {main program}
+  For Counter := 1 To 10 Do
+    Begin
+      TestBed[Counter] := 0;
+    End; {The output counter is initialized.}
+  For Counter := 1 To NUMBERofTrials Do
+    Begin
+      Temp := trunc(1 + ((Random(Counter)) * 10)); {i.e. 1 .. 10}
+      TestBed[Temp] := TestBed[Temp] + 1;
+    End; {We've counted appearances of 10,000 numbers.}
+  writeln('Random test distribution:');
+  For Counter := 1 To 10 Do
+    Begin
+      write(Counter: 3);
+      write('''s')
+    End; {The heading is printed.}
+  writeln;
+  For Counter := 1 To 10 Do
+    Begin
+      write(TestBed[Counter]: 5)
+    End; {The counts are printed.}
+  writeln
+End. {TestRand}
